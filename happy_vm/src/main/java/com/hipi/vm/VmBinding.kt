f4daeaf00de9609079c74f4fe92d563e64a355a9
manjiale <1013666753@qq.com>
2020-01-13 15:54:08 +0800

    bug

--- happy_vm/src/main/java/com/hipi/vm/VmBinding.kt
+++ happy_vm/src/main/java/com/hipi/vm/VmBinding.kt
@@ -4,27 +4,37 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.hapi.vmannotation.buidFuncName
 import com.hapi.vmannotation.suffix
+import java.lang.Exception
 
 
 object VmBinding {
 
-     fun bindVm(target: androidx.fragment.app.FragmentActivity){
-          val classs = target.javaClass
-          val claName = classs.name + suffix
-          val clazz = Class.forName(claName)
-          val bindMethod = clazz.getMethod(buidFuncName, target::class.java)
-          val ob = clazz.newInstance()
-          bindMethod.invoke(ob, target)
+
+     fun bindVm(target: FragmentActivity) {
+          try {
+               val classs = target.javaClass
+               val claName = classs.name + suffix
+               val clazz = Class.forName(claName)
+               val bindMethod = clazz.getMethod(buidFuncName, target::class.java)
+               val ob = clazz.newInstance()
+               bindMethod.invoke(ob, target)
+          } catch (e: ClassNotFoundException) {
+               e.printStackTrace()
+          }
      }
 
 
-     fun bindVm(target: androidx.fragment.app.Fragment){
-          val classs = target.javaClass
-          val claName = classs.name + suffix
-          val clazz = Class.forName(claName)
-          val bindMethod = clazz.getMethod(buidFuncName, target::class.java)
-          val ob = clazz.newInstance()
-          bindMethod.invoke(ob, target)
+     fun bindVm(target: Fragment) {
+          try {
+               val classs = target.javaClass
+               val claName = classs.name + suffix
+               val clazz = Class.forName(claName)
+               val bindMethod = clazz.getMethod(buidFuncName, target::class.java)
+               val ob = clazz.newInstance()
+               bindMethod.invoke(ob, target)
+          } catch (e: ClassNotFoundException) {
+               e.printStackTrace()
+          }
      }
 
 
